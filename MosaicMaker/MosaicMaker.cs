9e257ed5a80939cf731b5ab55d426d0d60ea0cc7
Donna Malayeri <lindydonna@gmail.com>
2017-10-03 18:04:42 -0700

    Cherry pickSend URL of mosaic in Location header (cherry pick 8ffb364)

--- MosaicMaker/MosaicMaker.cs
+++ MosaicMaker/MosaicMaker.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
@@ -34,11 +35,21 @@ namespace MosaicMaker
         [FunctionName("RequestMosaic")]
         public static string RequestImageProcessing(
             [HttpTrigger(AuthorizationLevel.Anonymous, new string[] { "POST" })] MosaicRequest input,
-            [Queue("generate-mosaic")] out MosaicRequest queueOutput,
+            [Queue("generate-mosaic")] ICollector<MosaicRequest> collector,
             TraceWriter log)
         {
-            queueOutput = input;
-            return "Mosaic request queued";
+            if (string.IsNullOrEmpty(input.OutputFilename))
+                input.OutputFilename = $"{Guid.NewGuid()}.jpg";
+
+            collector.Add(input);
+
+            var response = new HttpResponseMessage(HttpStatusCode.Accepted);
+            var storageURL = Environment.GetEnvironmentVariable("STORAGE_URL");
+            var outputContainerName = Environment.GetEnvironmentVariable("output-container");
+
+            response.Headers.Location = new Uri($"{storageURL}{outputContainerName}/{input.OutputFilename}");
+
+            return response;
         }
 
         [FunctionName("Settings")]
